2f4fde050662560739d30b065aaf1b202f2599ce
Mohamed Abo Full <mrabofull@gmail.com>
2025-03-24 20:26:33 +0200

    c

--- Frontend/app/_layout.jsx
+++ Frontend/app/_layout.jsx
@@ -5,20 +5,23 @@
 // }
 
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <Stack>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+        {/* <Stack>
           <Stack.Screen name="index" options={{ headerShown: false }} />
-          {/* <Stack.Screen
+          <Stack.Screen
             name="notifications"
             options={{ title: "Notifications", headerShown: false }}
-          /> */}
-        </Stack>
-        
+          />
+        </Stack> */}
+        <StatusBar style="light" />
+
+        <Stack screenOptions={{ headerShown: false }} />
       </SafeAreaView>
     </SafeAreaProvider>
   );
