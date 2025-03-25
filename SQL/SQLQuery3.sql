33685c873b778202c7666cd1e404742f2bb75b59
Mohamed Abo Full <mrabofull@gmail.com>
2025-03-25 15:11:09 +0200

    c

--- SQL/SQLQuery3.sql
+++ SQL/SQLQuery3.sql
@@ -9,3 +9,7 @@ VALUES
 ('Avi', 'Cohen', '1995-06-12', 'avi.cohen@example.com', 'hashedpassword1', 1, 123, '', 'Sports enthusiast, loves football', 'M'),
 ('Yaara', 'Levi', '1992-11-25', 'yaara.levi@example.com', 'hashedpassword2', 2, 456, '', 'Basketball player, traveler', 'F'),
 ('Oren', 'Shwartz', '1988-03-09', 'oren.shwartz@example.com', 'hashedpassword3', 3, 789, '', 'Tech lover, enjoys swimming', 'M');
+
+INSERT INTO CityOrganizers (UserId, CityId)
+VALUES
+(8, 1)
