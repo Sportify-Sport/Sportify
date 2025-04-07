67a83c5d7d5382231499d0df19ab45439e328ad4
Mohamed Abo Full <mrabofull@gmail.com>
2025-04-07 04:20:00 +0300

    small api name change

--- Backend/Controllers/GroupsController.cs
+++ Backend/Controllers/GroupsController.cs
@@ -32,23 +32,23 @@ namespace Backend.Controllers
         //    }
         //}
 
-        [HttpGet("group/{groupId}/isAdmin")]
-        [Authorize(Roles = "User")]
-        public IActionResult IsUserGroupAdmin(int groupId)
-        {
-            try
-            {
-                var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        //[HttpGet("group/{groupId}/isAdmin")]
+        //[Authorize(Roles = "User")]
+        //public IActionResult IsUserGroupAdmin(int groupId)
+        //{
+        //    try
+        //    {
+        //        var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
 
-                bool isAdmin = Group.IsUserGroupAdmin(userId, groupId);
+        //        bool isAdmin = Group.IsUserGroupAdmin(userId, groupId);
 
-                return Ok(new { success = true, isAdmin });
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new { success = false, message = $"An error occurred: {ex.Message}" });
-            }
-        }
+        //        return Ok(new { success = true, isAdmin });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(500, new { success = false, message = $"An error occurred: {ex.Message}" });
+        //    }
+        //}
 
         [AllowAnonymous]
         [HttpGet("GetGroups")]
@@ -77,7 +77,7 @@ namespace Backend.Controllers
 
 
         [AllowAnonymous]
-        [HttpGet("getGroupDetails/{groupId}")]
+        [HttpGet("{groupId}")]
         public IActionResult GetGroupDetails(int groupId)
         {
             try
