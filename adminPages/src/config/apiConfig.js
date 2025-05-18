11c10db49bcb780970839405a535cdfdf8bb7767
Mohamad Laham <lhamm@gmail.com>
2025-05-18 16:55:14 +0300

    removing admin folder
    removing admin folder from sportify project

--- adminPages/src/config/apiConfig.js
+++
@@ -1,8 +0,0 @@
-const getApiBaseUrl = () => {
-    return process.env.NODE_ENV === 'development'
-      ? 'https://localhost:7059' // Development (Local)
-      : 'https://your-production-api.com'; // Production
-  };
-  
-  export default getApiBaseUrl;
-  
