87c17416dfe9fde667ef4d7633bba657bcda48eb
Mohamed Abo Full <mrabofull@gmail.com>
2025-06-07 19:10:25 +0300

    Bug Fix - Updated dependencies - Backend
    Updated MailKit from "3.4.3" To "4.12.1"
    Updated MimeKit from "3.4.3" To "4.12.1"

--- Backend/Models/AuthResponse.cs
+++ Backend/Models/AuthResponse.cs
@@ -4,6 +4,6 @@
     {
         public string AccessToken { get; set; }
         public string RefreshToken { get; set; }
-        public bool IsEmailVerified { get; set; }
+        // public bool IsEmailVerified { get; set; }
     }
 }
